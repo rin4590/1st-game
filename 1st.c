8bd0ca2dfac7586de3c7f9e43f43c19971750b4d
yezune <yezune@gmail.com>
2016-05-02 02:24:36 +0900

    add number game

--- 1st.c
+++ 1st.c
@@ -1,52 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-
-int distance(int theta){
-    
-    int velocity = 10; // m/s
-    int time = 0;
-    int g = 
-
-    printf("theta is %d\n", theta );
-    return random() % 100;
-}
+#include <stdio.h>  // printf, scanf
+#include <stdlib.h> // random()
 
 
 int target()
 {
-    return random() % 100;
+    return random() % 50;
 }
 
-int main(int argc, char **args)
+int main(void)
 {
-
     // 1. setting target 
 
-    // for(int i=0; i< 10; i++){
-    //     printf("target %d\n", target());
-    // }
+    int num=0;
+    int yn=0;
+    printf("-------숫자 맞추기 게임을 시작합니다!!-------\n\n\n\n\n");
 
     int tgt = target();
-    printf("target %d\n", tgt);
-
-    // 2. guessing target using theta 
-    while(1)
+    do 
     {
-        int theta=0;
-        printf("input theta:");
-        scanf("%d", &theta);
+        
+        printf("값을 입력해주세요! :");
+        scanf("%d",&num);
 
-        int dist = distance(theta);
 
-        printf("distance is %d!\n", dist);
-
-        if (dist == tgt){
-            printf("ok! it's correct!");
-        }else{
-            printf("mismatch!");
+        if(tgt == num){
+            printf("정답입니다!\n");
+            printf("게임을 계속하겠습니까? (1/0):");
+            scanf("%d",&yn);
+            if(yn==1){
+                printf("게임을 종료합니다.\n");
+                exit(0);
+            }else{
+                tgt = target();
+            }
+            printf("\n");
         }
-    }
+        else if(tgt<num)
+        {
+            printf("%d는 정답보다 큽니다.\n",num);
+        }
+        else 
+        {
+            printf("%d는 정답보다 작습니다.\n",num);
+        }
+
+    }while(1);
+
     return 0;
 }
